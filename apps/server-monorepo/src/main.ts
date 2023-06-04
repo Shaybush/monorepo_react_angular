cfeb02c282358a7d193374e3c8c677d68aed8d7f
Shay Bushary <shay.bushary@1touch.io>
2023-06-04 18:17:17 +0300

    add server config

--- apps/server-monorepo/src/main.ts
+++ apps/server-monorepo/src/main.ts
@@ -1,13 +1,23 @@
-import express from 'express';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require("express");
+const http = require("http");
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
+
+app.get('/users', (req, res) => {
+  res.send({ message: 'Hello API' });
+});
+
+const server = http.createServer(app);
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
