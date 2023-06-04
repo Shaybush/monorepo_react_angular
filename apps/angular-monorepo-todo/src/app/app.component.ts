cbebe1b36069c993c955665c1697e7a4f25b4f6f
Shay Bushary <shay.bushary@1touch.io>
2023-06-04 14:10:10 +0300

    Initial commit

---
+++ apps/angular-monorepo-todo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-monorepo-todo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-monorepo-todo';
+}
