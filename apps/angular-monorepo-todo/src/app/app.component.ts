aa7783267d8d3e42799d2d75cbe6411aa75d6946
Shay Bushary <shay.bushary@1touch.io>
2023-06-04 15:10:09 +0300

    monorepo step1 : install + environment setup

--- apps/angular-monorepo-todo/src/app/app.component.ts
+++ apps/angular-monorepo-todo/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { sumTwoNumbers } from '@angular-monorepo-todo/utils';
 @Component({
   selector: 'angular-monorepo-todo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-monorepo-todo';
+  num1 = 2;
+  num2 = 3;
+  ngOnInit(): void {
+    console.log(sumTwoNumbers(this.num1, this.num2))
+  }
 }
---
+++ apps/shop/src/app/app.tsx
@@ -0,0 +1,14 @@
+import './app.module.scss';
+import { sumTwoNumbers } from '@angular-monorepo-todo/utils';
+
+export function App() {
+  console.log(sumTwoNumbers(4, 5))
+  return (
+    <div className='react-app-wrapper'>
+      <div className='header'>React App</div>
+      <a href="http://localhost:4200">move to angular app</a>
+    </div>
+  );
+}
+
+export default App;
